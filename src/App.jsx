12da4d1acb0139928976e3b3ed8e1237dc370738
mahmudul1024 <mahmudul.info.bd@gmail.com>
2025-02-28 17:10:51 +0100

    logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-  <>
-  <button className="btn btn-active">Default</button>
-<button className="btn btn-active btn-neutral">Neutral</button>
-<button className="btn btn-active btn-primary">Primary</button>
-  </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
